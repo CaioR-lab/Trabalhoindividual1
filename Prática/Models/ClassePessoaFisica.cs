aa2846d9d5b9939c66b2794508410233548ffab7
caio <caio.mateus.r@gmail.com>
2021-07-16 15:11:53 -0300

    classes criadas, pratica pronta

---
+++ Prática/Models/ClassePessoaFisica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pratica.Models;
+
+namespace Pratica.Models
+{
+    public class ClassePessoaFisica : ClassePessoa
+    {
+
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public ClasseEndereco Endereco { get; set; }
+
+    }
+}
---
+++ Prática/Models/ClassePessoaJuridica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratica.Models
+{
+    public class ClassePessoaJuridica : ClassePessoa
+    {
+        public string CNPJ { get; set; }
+        public ClasseEndereco Endereco { get; set; }
+    }
+}
